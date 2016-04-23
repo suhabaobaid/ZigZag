70d22d132df674f5842512b72ea2449154e39593
suhabaobaid <serenab275@gmail.com>
2016-04-23 21:56:37 +0300

    Initial Commit

---
+++ ZigZag_hackathon_AIU2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZigZag_hackathon_AIU2016
+//
+//  Created by Suha Baobaid on 23/04/16.
+//  Copyright © 2016 Suha Baobaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ZigZag_hackathon_AIU2016Tests/ZigZag_hackathon_AIU2016Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ZigZag_hackathon_AIU2016Tests.swift
+//  ZigZag_hackathon_AIU2016Tests
+//
+//  Created by Suha Baobaid on 23/04/16.
+//  Copyright © 2016 Suha Baobaid. All rights reserved.
+//
+
+import XCTest
+@testable import ZigZag_hackathon_AIU2016
+
+class ZigZag_hackathon_AIU2016Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
